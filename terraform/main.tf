8f12a502a885725ab843e1c38d8031902cb9e98d
Raul Naupari <raulnq@gmail.com>
2025-07-01 15:50:59 -0500

    cloudflare tunnel

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,30 +21,12 @@ resource "google_compute_instance" "free_tier_vm" {
     network = "default"
     access_config {}
   }
-  tags = ["http-server"]
   metadata = {
     startup-script = file("${path.module}/startup-script.sh")
   }
 }
 
-resource "google_compute_firewall" "allow_http" {
-  project = var.project_id
-  name    = "allow-http-ingress"
-  network = "default"
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-  target_tags = ["http-server"]
-  source_ranges = ["0.0.0.0/0"]
-}
-
 variable "project_id" {
   type        = string
   description = "The GCP project ID to deploy resources into."
-}
-
-output "vm_external_ip" {
-  description = "The external IP address of the web server VM."
-  value       = google_compute_instance.free_tier_vm.network_interface[0].access_config[0].nat_ip
 }
--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -5,4 +5,18 @@ apt-get update -y
 apt-get install -y nginx
 systemctl start nginx
 systemctl enable nginx
+wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+dpkg -i cloudflared-linux-amd64.deb
+mkdir -p /etc/cloudflared/
+cat > /etc/cloudflared/config.yml << EOF
+tunnel: <MY_TUNNEL_ID>
+credentials-file: /root/.cloudflared/<MY_TUNNEL_ID>.json
+ingress:
+  - hostname: nginx.<MY_DOMAIN>
+    service: http://127.0.0.1:80
+  - service: http_status:404
+EOF
+cloudflared service install <MY_TUNNEL_TOKEN> --config /etc/cloudflared/config.yml
+systemctl start cloudflared
+systemctl enable cloudflared
 echo "=== Startup script completed at $(date) ==="
